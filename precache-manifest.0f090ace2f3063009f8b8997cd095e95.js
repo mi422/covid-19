84f27a2d712a7fb8a298e08977f16f7dcfda7e3d
omayma <missaomayma@gmail.com>
2020-08-05 00:24:43 +0100

    Updates

---
+++ precache-manifest.0f090ace2f3063009f8b8997cd095e95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "319991aeb029623377b67d9a32334b3e",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "4ea99ca78a3da0bc7df5",
+    "url": "/covid-19/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "44e1f0e683a86b2479f9",
+    "url": "/covid-19/static/css/main.bb328c32.chunk.css"
+  },
+  {
+    "revision": "4ea99ca78a3da0bc7df5",
+    "url": "/covid-19/static/js/2.393ef4e6.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid-19/static/js/2.393ef4e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44e1f0e683a86b2479f9",
+    "url": "/covid-19/static/js/main.4e0400d7.chunk.js"
+  },
+  {
+    "revision": "ce18e44a6f281162c1d1",
+    "url": "/covid-19/static/js/runtime-main.2c53739b.js"
+  }
+]);
